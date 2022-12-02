d4587e3df8092248295ad7f7375af15237d285c2
Daurius Swarts <ali@openmindi.co.za>
2022-12-02 19:20:24 +0200

    Home Update

--- nk-docs-v2/app/page.tsx
+++ nk-docs-v2/app/page.tsx
@@ -5,15 +5,15 @@ type Props = {}
 export default function Home({}: Props) {
   return (
     <div>
-    <section className="dark:bg-gray-800 dark:text-gray-100">
-        <div className="container mx-auto flex flex-col items-center px-4 py-16 text-center md:py-32 md:px-10 lg:px-32 xl:max-w-3xl">
-            <h1 className="text-4xl font-bold leading-none sm:text-5xl">Quisquam necessita vel
-                <span className="dark:text-[#caa969]">laborum doloribus</span>delectus
+    <section className="dark:bg-[#080a0b] dark:text-gray-100">
+        <div className="container mx-auto h-screen flex flex-col items-center px-4 py-16 text-center md:py-32 md:px-10 lg:px-32 xl:max-w-3xl">
+            <h1 className="text-4xl font-bold leading-none sm:text-2xl uppercase">Documentation
             </h1>
-            <p className="px-8 mt-8 mb-12 text-lg">Cupiditate minima voluptate temporibus quia? Architecto beatae esse ab amet vero eaque explicabo!</p>
+              <h2 className="dark:text-[#caa969]">NAMKA CRYPTO DRAW</h2>
+            <p className="px-8 mt-8 mb-12 text-sm">Web3 Crypto Draw Platform for testing</p>
             <div className="flex flex-wrap justify-center">
-                <button className="px-8 py-3 m-2 text-lg font-semibold rounded dark:bg-[#080a0b] dark:text-[#caa969]">Get started</button>
-                <button className="px-8 py-3 m-2 text-lg border rounded dark:text-gray-50 dark:border-gray-700">Learn more</button>
+              <a href="/start"><button className="px-8 py-3 m-2 text-lg font-semibold rounded dark:bg-[#2c2b2b] dark:text-[#caa969]">Get started</button></a>
+                <a href="https://nk-crypto-draw.vercel.app/" target="_blank"><button className="px-8 py-3 m-2 text-lg border rounded dark:text-gray-50 dark:border-gray-700">Crypto Draw</button></a>
             </div>
         </div>
     </section>
