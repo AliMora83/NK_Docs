cdf31b759b84d43dc6642af2a2e1b958a81633c8
Daurius Swarts <ali@openmindi.co.za>
2022-12-02 19:04:06 +0200

    Looking good

---
+++ nk-docs-v2/app/internal/internal.tsx
@@ -0,0 +1,34 @@
+import React from 'react'
+
+type Props = {}
+
+function Internals({}: Props) {
+  return (
+    <div className='mb-10'>
+        <div className="text-gray-700 text-left max-w-md justify-center md:max-w-2xl lg:max-w-4xl mx-auto" id='sdk'>
+                  <h4 className='pt-10 pb-5 text-gray-800 font-semibold'>
+                     SDK Guide
+                  </h4>
+                  <p>
+                  Minimum hardware and software requirements. Rem reprehenderit ullam placeat quis deserunt eligendi, nemo ipsa eaque natus tempore alias, corrupti, quas ea? Itaque, vitae cum. Vitae necessitatibus optio eum totam perspiciatis soluta delectus voluptatem ratione, maiores quam architecto ea nulla repellendus fuga nemo esse corporis! Molestias a, atque exercitationem ad alias eius expedita harum debitis non consequuntur maiores illum quibusdam placeat eos velit tenetur, ipsum asperiores labore? Rem blanditiis adipisci ab natus provident quibusdam dignissimos odit! Soluta ullam quisquam unde odio vel optio nobis enim a dolor, harum aliquam fuga labore similique dolorem nesciunt, molestias ratione repudiandae dignissimos.</p>
+                  </div>
+                  <div className="text-gray-700 text-left max-w-md justify-center md:max-w-2xl lg:max-w-4xl mx-auto" id='api'>
+                  <h4 className='pt-20 pb-5 text-gray-800 font-semibold'>
+                     API Guide
+                  </h4>
+                  <p>
+                  Lorem ipsum dolor, sit amet consectetur adipisicing elit. Rem reprehenderit ullam placeat quis deserunt eligendi, nemo ipsa eaque natus tempore alias, corrupti, quas ea? Itaque, vitae cum. Vitae necessitatibus optio eum totam perspiciatis soluta delectus voluptatem ratione, maiores quam architecto ea nulla repellendus fuga nemo esse corporis! Molestias a, atque exercitationem ad alias eius expedita harum debitis non consequuntur maiores illum quibusdam placeat eos velit tenetur, ipsum asperiores labore? Rem blanditiis adipisci ab natus provident quibusdam dignissimos odit! Soluta ullam quisquam unde odio vel optio nobis enim a dolor, harum aliquam fuga labore similique dolorem nesciunt, molestias ratione repudiandae dignissimos.</p>
+                  </div>
+                  <div className="text-gray-700 text-left max-w-md justify-center md:max-w-2xl lg:max-w-4xl mx-auto" id='support'>
+                  <h4 className='pt-20 pb-5 text-gray-800 font-semibold'>
+                     Support
+                  </h4>
+                  <p>
+                  Lorem contact details and where to look for more information or assistance. Implement localization. People like getting information in their own language. So, take care of your global audience and consider using location data to adapt your content. Localization can also mean converting units of measurement, currencies, date and address formats, and so on. nemo ipsa eaque natus tempore alias, corrupti, quas ea? Itaque, vel optio nobis enim a dolor, harum aliquam fuga labore similique dolorem nesciunt, molestias ratione repudiandae dignissimos.</p>
+                  </div>
+                  
+    </div>
+  )
+}
+
+export default Internals
--- nk-docs-v2/app/navmenu.tsx
+++ nk-docs-v2/app/navmenu.tsx
@@ -9,10 +9,9 @@ export default function Navmenu({}: Props) {
         <aside className="h-full p-6 sm:w-60 dark:bg-[#080a0b] dark:text-gray-100 fixed">
             <nav className="space-y-8 text-sm pt-5">
               <div className="space-y-2">
-                <h2 className="text-sm font-semibold tracking-widest uppercase dark:text-gray-400">Getting Started</h2>
-                <div className="flex flex-col space-y-1">
-                  <Link href="start">Start here</Link>
-                  
+                <Link href="start">
+                <h2 className="text-sm font-semibold tracking-widest uppercase dark:text-gray-400">Getting Started</h2></Link>
+                <div className="flex flex-col space-y-1">                  
                   <a className='a' rel="noopener noreferrer" href="#what">What is it?</a>
                   <a className='a' rel="noopener noreferrer" href="#features">Features</a>
                   <a className='a' rel="noopener noreferrer" href="#wallet">Web3 Wallet</a>
@@ -20,23 +19,21 @@ export default function Navmenu({}: Props) {
                 </div>
               </div>
               <div className="space-y-2">
-                <h2 className="text-sm font-semibold tracking-widest uppercase dark:text-gray-400">General Use</h2>
+                <Link href="user">
+                <h2 className="text-sm font-semibold tracking-widest uppercase dark:text-gray-400">User Guide</h2></Link>
                 <div className="flex flex-col space-y-1">
-                  <Link href="user">User Guide</Link>
                   <a className='a' rel="noopener noreferrer" href="#visit">Visit us</a>
                   <a className='a' rel="noopener noreferrer" href="#info">Browse Info</a>
                   <a className='a' rel="noopener noreferrer" href="#draw">Go to Draw</a>
-                  <a className='a' rel="noopener noreferrer" href="#tickets">Buy Tickets</a>
-                  <a className='a' rel="noopener noreferrer" href="#wait">Wait for Draw</a>
                   <a className='a' rel="noopener noreferrer" href="#check">Check Winnings</a>
                   <a className='a' rel="noopener noreferrer" href="#faq">FAQ's</a>
                 </div>
               </div>
               <div className="space-y-2">
-                <h2 className="text-sm font-semibold tracking-widest uppercase dark:text-gray-400">Corporate</h2>
+                <Link href="corp">
+                <h2 className="text-sm font-semibold tracking-widest uppercase dark:text-gray-400">Corporate Guide</h2></Link>
                 <div className="flex flex-col space-y-1">
                   <a className='a' rel="noopener noreferrer" href="#addToWeb">Add to Website</a>
-                  <a className='a' rel="noopener noreferrer" href="#hero">Add Hero Banner</a>
                   <a className='a' rel="noopener noreferrer" href="#cta">Add Call-to-Action</a>
                   <a className='a' rel="noopener noreferrer" href="#web3">Connect to Web3</a>
                   <a className='a' rel="noopener noreferrer" href="#whitelabel">Whitelabel Requirements</a>
@@ -44,7 +41,8 @@ export default function Navmenu({}: Props) {
                 </div>
               </div>
               <div className="space-y-2">
-                <h2 className="text-sm font-semibold tracking-widest uppercase dark:text-gray-400">Internal Team</h2>
+                <Link href="internal">
+                <h2 className="text-sm font-semibold tracking-widest uppercase dark:text-gray-400">Internal Guide</h2></Link>
                 <div className="flex flex-col space-y-1">
                   <a className='a' rel="noopener noreferrer" href="#sdk">SDK Guide</a>
                   <a className='a' rel="noopener noreferrer" href="#api">API Guide</a>
